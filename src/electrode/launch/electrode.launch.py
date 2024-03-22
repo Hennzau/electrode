79b8dae79223764c3a517fa6629d9051fd73b9b1
Hennzau <dev@enzo-le-van.fr>
2024-03-22 19:04:38 +0100

    vision rework

--- src/electrode/launch/electrode.launch.py
+++ src/electrode/launch/electrode.launch.py
@@ -51,7 +51,7 @@ ARGUMENTS = [
     ),
 
     DeclareLaunchArgument('topic_whitelist',
-        default_value=['["/camera/image_raw/compressed","/camera/camera_info","/cerebri/out/status","/debugImage", "/lidar/scan","/tf"]'],
+        default_value=['["/camera/image_raw/compressed","/camera/camera_info","/cerebri/out/status","/vision/image_raw/compressed", "/lidar/scan","/tf"]'],
         description='topic_whitelist for foxglove'
     ),
 
